94c02708ca0e381c2e03ea1dc4d8f5ee63d3583d
AkiraShingu <shinguakira1022@gmail.com>
2025-07-09 03:56:39 +0900

    add vercel and azure and aws config

--- src/tests/projects.test.ts
+++ src/tests/projects.test.ts
@@ -7,26 +7,20 @@ describe('GET /api/projects', () => {
   it('should respond with a 200 status code and the Japanese projects data by default', async () => {
     const response = await request(app).get('/api/projects');
     expect(response.status).toBe(200);
-    const expectedProjects = projects.map(project => {
-      const lang = 'ja';
-      return {
-        technologies: project.technologies,
-        ...(lang === 'en' ? project.en : project.ja)
-      };
-    });
+    const expectedProjects = projects.map(project => ({
+      technologies: project.technologies,
+      ...project.ja
+    }));
     expect(response.body).toEqual(expectedProjects);
   });
 
   it('should respond with a 200 status code and the English projects data when lang=en', async () => {
     const response = await request(app).get('/api/projects?lang=en');
     expect(response.status).toBe(200);
-    const expectedProjects = projects.map(project => {
-      const lang = 'en';
-      return {
-        technologies: project.technologies,
-        ...(lang === 'en' ? project.en : project.ja)
-      };
-    });
+    const expectedProjects = projects.map(project => ({
+      technologies: project.technologies,
+      ...project.en
+    }));
     expect(response.body).toEqual(expectedProjects);
   });
 });
